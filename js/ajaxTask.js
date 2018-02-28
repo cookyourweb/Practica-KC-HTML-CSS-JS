7a9ec9e006cbb7cc627103a229c2e9f0a98f880e
Veronica Serna <verserper@gmail.com>
2018-02-28 03:57:41 +0100

    Ultimos arreglos para entrega de practica a falta de meter seo

--- js/ajaxTask.js
+++ js/ajaxTask.js
@@ -4,11 +4,11 @@ var drawTasks = function() {
 	$('#taskContainer').empty();
 
 	if (tasks.length == 0) {
-		$('#taskContainer').append("<li>No hay tareas pendientes</li>");
+		$('#taskContainer').append("<li style='list-style:none;'>No hay tareas pendientes</li>");
 	} else {
 		var contentToAdd = '';
 		for (var i = 0; i < tasks.length; i++) {
-			contentToAdd += '<li class="task-item">' + tasks[i].name + '<button class="deleteTask" data-task-id="' + tasks[i].id + '">Eliminar</button></li>'
+			contentToAdd += '<li style="list-style:none; height:45px; " class="task-item" >' + tasks[i].name + '<button class="deleteTask" data-task-id="' + tasks[i].id + '"><span style="font-size:1.5em; "><i class="fa fa-times-circle"></i></span> Eliminar</button></li>'
 		}
 		$('#taskContainer').append(contentToAdd);
 	}
